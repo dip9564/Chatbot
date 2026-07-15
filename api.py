faa1a81588c7107877b73b4e89d264e1e3a5d7bb
dip9564 <dipmondal9564@gmail.com>
2026-07-15 15:39:30 +0530

    key

--- api.py
+++ api.py
@@ -1,14 +1,13 @@
 import os
 import streamlit as st
-from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
-try :
-    api_key = os.getenv("GEMINI_API_KEY")
-except:
-    api_key = st.secrets["GEMINI_API_KEY"]
+# api_key = os.getenv("GEMINI_API_KEY")
+
+api_key = st.secrets["GEMINI_API_KEY"]
 
 
 client = genai.Client(api_key=api_key)
